87b0498993a71713afc60482574a0900128390f1
Saúl Ibarra Corretgé <s@saghul.net>
2022-06-16 14:44:51 +0200

    Print fourcc

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ clean:
 		rm *.o
 
 avflist: main.o
-		$(CC) -Wall -framework Foundation -framework AVFoundation -o $@ $(LDFLAGS) $^
+		$(CC) -Wall -framework Foundation -framework AVFoundation -framework CoreMedia -o $@ $(LDFLAGS) $^
 
 %.o: %.m
 		$(CC) -Wall -c -fobjc-arc -o $@ $(OBJCFLAGS) $<
--- main.m
+++ main.m
@@ -6,8 +6,17 @@
 //
 
 #import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
 #import <Foundation/Foundation.h>
 
+#define FOURCC_STR(code)                                     \
+    (char[5])                                               \
+    {                                                       \
+	(char)((code >> 24) & 0xFF),         \
+        (char)((code >> 16) & 0xFF), \
+        (char)((code >> 8) & 0xFF),  \
+        (char)(code & 0xFF), 0       \
+    }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -19,7 +28,8 @@ int main(int argc, const char * argv[]) {
         for (AVCaptureDevice *device in devices) {
             NSLog(@"%@ %@ %@", device.localizedName, device.uniqueID, device.modelID);
             for (AVCaptureDeviceFormat *format in device.formats) {
-                NSLog(@"\t %@", format);
+                FourCharCode fourcc = CMFormatDescriptionGetMediaSubType(format.formatDescription);
+                NSLog(@"\t %@ (%s)", format, FOURCC_STR(fourcc));
             }
         }
     }
